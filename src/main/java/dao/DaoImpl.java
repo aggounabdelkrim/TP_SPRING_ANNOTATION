652697f76d241f3490ebd469aa5dcf0ab7c07ea2
aggounabdelkrim <aggoun.abdelkrim@gmail.com>
2023-02-17 10:44:11 +0100

    first commit

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Karim-PC
+ */
+@Component
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        /**
+         * se connecter à la DDB récuper la donnée
+         */
+        System.out.println("Version base des données ...");
+        double temp = Math.random() * 40;
+        return temp;
+    }
+
+}
---
+++ src/main/java/pres/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package pres;
+
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Karim
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext("dao", "metier");
+        IMetier metier = context.getBean(IMetier.class);
+        System.out.println("Resultat---> " + metier.calcul());
+    }
+}
