326cbcbfc9a9716ab14709b6cf1b4058dd8c9737
aggounabdelkrim <aggoun.abdelkrim@gmail.com>
2023-03-02 09:45:10 +0100

    1

--- src/main/java/pres/Main.java
+++ src/main/java/pres/Main.java
@@ -6,7 +6,6 @@ package pres;
 import metier.IMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
